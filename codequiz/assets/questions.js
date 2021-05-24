ce7ecef62fd440130acee166ec6746e199e46076
Darren Watson <v_tec@live.com>
2021-05-23 20:25:36 -0500

    finally got all answers rendering

---
+++ codequiz/assets/questions.js
@@ -0,0 +1,47 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Which of the following is the correct syntax for an alert box?",
+    answer: "alert()",
+    options: [
+      "alertbox()",
+      "msg()",
+      "msgBox()",
+      "alert()"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which of the following is not a reserved word in JavaScript ?",
+    answer: "program",
+    options: [
+      "interface",
+      "throws",
+      "short",
+      "program"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which of these is short-hand for function?",
+    answer: "=>",
+    options: [
+      "=>",
+      "funct",
+      "function",
+      "<="
+    ]
+  },
+  {
+    numb: 4,
+    question: "What js function prints to the log?",
+    answer: "console.log()",
+    options: [
+      "print()",
+      "console.log()",
+      "return.log()",
+      "log()"
+    ]
+  }
+];
